36f6f9c1b5a08b7489ad3b36e7b38a32fc0bf19c
Franz Jeff Dignos <fdignos@isbx.com>
2023-07-07 10:37:01 +0800

    add state and interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import { Component } from "react";
+
+export interface Pokemon {
+  name: string,
+  level: number
+}
+
+class App extends Component {
+
+  state: { pokemons: Pokemon[] } = {
+    pokemons: []
+  };
+
+  componentDidMount(): void {
+    this.setState({
+      pokemons: [
+        {
+          name: 'Pikachu',
+          level: 10,
+        },
+        {
+          name: 'Balbasaur',
+          level: 12
+        }
+      ]
+    })
+  }
+
+
+  render() {
+
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
 }
 
 export default App;
