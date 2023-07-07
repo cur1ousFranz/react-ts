ef6da75f5dcb339457868c95f216c9ea70fc27fa
Franz Jeff Dignos <fdignos@isbx.com>
2023-07-07 10:46:12 +0800

    create form to create data

--- src/components/PokemonForm.tsx
+++ src/components/PokemonForm.tsx
@@ -14,9 +14,23 @@ export default class PokemonForm extends Component<FormProps> {
 
     render() {
         return (
-           <div>
-
-           </div>
+            <form className='pokemon-form'>
+                <div>
+                    <input
+                        onChange={(e) => this.setState({ name: e.target.value })}
+                        type="text"
+                        name="Name"
+                        className='name' />
+                </div>
+                <div>
+                    <input
+                        onChange={(e) => this.setState({ level: e.target.value })}
+                        type="number"
+                        name="Level"
+                        className='level' />
+                </div>
+                <button className='pokemon-submit'>Add Pokemon</button>
+            </form>
         )
     }
 }
